5429d267af3e5ef257b03631256acaa73a1b3bb9
Earl Jay Caoile <v-eacaoi@microsoft.com>
2020-01-13 08:59:53 -0800

    fixed spacing

--- edit-file.js
+++ edit-file.js
@@ -3,41 +3,40 @@
 const reader = require('./lib/reader.js');
 const writer = require('./lib/writer.js');
 const filePath = `${__dirname}/files/data/person.json`;
-const Validator = require('./lib/validator.js');
-const valObj = new Validator();
+
 const personRules = {
-    fields: {
-      firstName: { typing: 'string', required: true },
-      lastName: { typing: 'string', required: true },
-      hair: {
-        typing: 'object', required: true,
-        type: { typing: 'string', required: true },
-        color: { typing: 'string', required: true },
-      },
-      favoriteFoods: { typing: 'array', valueType: 'string' },
-      married: { typing: 'boolean', required: true },
-      kids: { typing: 'number', required: true }
+  fields: {
+    firstName: { typing: 'string', required: true },
+    lastName: { typing: 'string', required: true },
+    hair: {
+      typing: 'object', required: true,
+      type: { typing: 'string', required: true },
+      color: { typing: 'string', required: true },
     },
+    favoriteFoods: { typing: 'array', valueType: 'string' },
+    married: { typing: 'boolean', required: true },
+    kids: { typing: 'number', required: true },
+  },
 };
 
 let editHelper = (err, filePath) => {
+  if (err) { throw err; }
+  console.log('2: Received file path: ', filePath);
+  reader.readerWithCallback(filePath, (err, data) => {
     if (err) { throw err; }
-    console.log('2: Received file path: ', filePath);
-    reader.readerWithCallback(filePath, (err, data) => {
-        if (err) { throw err; }
-        console.log('Callback from fs read:', data);
-        console.log('Current marital status:', data.married);
-        writer.writerWithCallback(filePath, data, personRules, (innerErr, innerData) => {
-            if (innerErr) { throw innerErr };
-            console.log('Callback from fs write', innerData);
-            console.log(`Changed marital status to ${innerData.married}`);
-        });
+    console.log('Callback from fs read:', data);
+    console.log('Current marital status:', data.married);
+    writer.writerWithCallback(filePath, data, personRules, (innerErr, innerData) => {
+      if (innerErr) { throw innerErr; }
+      console.log('Callback from fs write', innerData);
+      console.log(`Changed marital status to ${innerData.married}`);
     });
-}
+  });
+};
 
 let editFile = (filePath, cb) => {
-    console.log('1: Entered editfile');
-    cb(!filePath ? 'Error: empty file' : undefined, filePath);
-}
+  console.log('1: Entered editfile');
+  cb(!filePath ? 'Error: empty file' : undefined, filePath);
+};
 
 editFile(filePath, editHelper);
